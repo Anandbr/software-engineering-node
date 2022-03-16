31cc918f78e40c0586b6c453a89ca30bca216caf
Anandbr <anandbr96@gmail.com>
2022-03-16 15:56:27 -0400

    commit

--- client/UserAxiosClient.ts
+++ client/UserAxiosClient.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import User from "../models/users/User";
 // axios.defaults.baseURL = 'http://localhost:4000/api';
+//need to change
 axios.defaults.baseURL = 'https://cs5500-01-sp22.herokuapp.com/api';
 
 const findAllUsers = async () =>
