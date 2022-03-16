a18ca6be6a795d5c913e34dcb30ad45026e018fa
Anandbr <anandbr96@gmail.com>
2022-03-16 18:53:27 -0400

    CORS modification origin true

--- server.ts
+++ server.ts
@@ -34,7 +34,7 @@ app.use(express.json());
 
 
 const corsOptions ={
-    origin:'*',
+    origin:true,
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 }
